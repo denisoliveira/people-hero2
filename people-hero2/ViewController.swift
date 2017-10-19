44b8ff650917df519f868fb2acc8eb5db2511869
Denis Wilson de Souza Oliveira <denis.oliveira@beblue.com.br>
2017-10-19 20:06:58 -0200

    Initial Commit

---
+++ people-hero2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  people-hero2
+//
+//  Created by Denis Oliveira on 19/10/17.
+//  Copyright © 2017 Denis Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
